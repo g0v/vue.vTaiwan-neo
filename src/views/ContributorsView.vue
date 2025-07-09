298fafcd203e03d47f921c4a9fc7a7218d4c7a78
Bestian Tang <109048+bestian@users.noreply.github.com>
2025-07-10 06:47:13 +0800

    Update ContributorsView.vue

--- src/views/ContributorsView.vue
+++ src/views/ContributorsView.vue
@@ -91,11 +91,13 @@ const communityContributors = ref<Contributor[]>([
   { id: '5', name: 'Peter Cui', contribution: 'contributors.contributions.contentManager', imgURL: 'https://ca.slack-edge.com/T02G2SXKM-UPHFV4KF0-8263b8a8cfdc-512' },
   { id: '6', name: 'Tofus Wang', contribution: 'contributors.contributions.uiuxDesign', imgURL: 'https://ca.slack-edge.com/T02G2SXKM-U01JXVDPS5R-5a1d6aace86d-512' },
   { id: '7', name: 'Tofus Wang', contribution: 'contributors.contributions.frontendDev', imgURL: 'https://ca.slack-edge.com/T02G2SXKM-U01JXVDPS5R-5a1d6aace86d-512' },
-  { id: '8', name: 'Tim', contribution: 'contributors.contributions.frontendDev', imgURL: 'https://ca.slack-edge.com/T02G2SXKM-U05BY0RLJM7-ab97abff7b44-512' },
-  { id: '9', name: 'Bestian Tang', contribution: 'contributors.contributions.frontendDev', imgURL: 'https://ca.slack-edge.com/T02G2SXKM-U053N6CAQ-338c6ec3f9d7-512' },
-  { id: '10', name: 'Bestian Tang', contribution: 'contributors.contributions.backendDev', imgURL: 'https://ca.slack-edge.com/T02G2SXKM-U053N6CAQ-338c6ec3f9d7-512' },
-  { id: '11', name: 'Nobody', contribution: 'contributors.contributions.dataAnalysis' },
-  { id: '12', name: 'Nobody', contribution: 'contributors.contributions.translationSupport' },
+  { id: '8', name: 'Joey Yu', contribution: 'contributors.contributions.uiuxDesign', imgURL: 'https://ca.slack-edge.com/T02G2SXKM-U0638P8FJ82-5ee2c8a9ae73-192' },
+  { id: '9', name: 'Tim', contribution: 'contributors.contributions.frontendDev', imgURL: 'https://ca.slack-edge.com/T02G2SXKM-U05BY0RLJM7-ab97abff7b44-512' },
+  { id: '10', name: 'Anan', contribution: 'contributors.contributions.frontendDev', imgURL: 'https://ca.slack-edge.com/T02G2SXKM-U08U8MNTS0M-d68838168b5e-192' },
+  { id: '11', name: 'Bestian Tang', contribution: 'contributors.contributions.frontendDev', imgURL: 'https://ca.slack-edge.com/T02G2SXKM-U053N6CAQ-338c6ec3f9d7-512' },
+  { id: '12', name: 'Bestian Tang', contribution: 'contributors.contributions.backendDev', imgURL: 'https://ca.slack-edge.com/T02G2SXKM-U053N6CAQ-338c6ec3f9d7-512' },
+  { id: '13', name: 'Nobody', contribution: 'contributors.contributions.dataAnalysis' },
+  { id: '14', name: 'Nobody', contribution: 'contributors.contributions.translationSupport' },
 
 ])
 </script>
