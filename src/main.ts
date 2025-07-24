fdcbcd386c585f181f7013acf6dbd4572b829ec5
Bestian Tang <109048+bestian@users.noreply.github.com>
2025-07-25 06:43:20 +0800

    安裝Google 分析，close #64

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,16 @@ import router from './router'
 import i18n from './i18n'
 import './style.css'
 
+import VueGtag from 'vue-gtag-next'
+
 const app = createApp(App)
 
 app.use(router)
 app.use(i18n)
+app.use(VueGtag, {
+  property: {
+    id: 'G-PJ396EXSTX'
+  }
+})
 
 app.mount('#app')
