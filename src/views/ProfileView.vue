0470addc9d66db094f818c85acca1270c33fe2fe
Bestian Tang <109048+bestian@users.noreply.github.com>
2025-08-04 06:16:48 +0800

    先讓ID不顯示，減少使用者雜訊。work on #61

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -41,12 +41,12 @@
               {{ user.email }}
             </div>
           </div>
-          <div class="md:col-span-2">
+          <!-- <div class="md:col-span-2">
             <label class="block text-sm font-medium text-gray-700 mb-2">用戶ID</label>
             <div class="px-3 py-2 bg-gray-50 border border-gray-300 rounded-md text-gray-800 font-mono text-sm">
               {{ user.uid }}
             </div>
-          </div>
+          </div> -->
         </div>
 
         <!-- 操作按鈕 -->
